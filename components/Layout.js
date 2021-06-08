cdfa068ace3c9ccce9e880a2f99cdabc3cdd0fb4
Neto Pompeu <neto1@hotmail.fr>
2021-06-08 20:25:13 +0200

    Design the footer

--- components/Layout.js
+++ components/Layout.js
@@ -23,7 +23,7 @@ export default function Layout({ children, page }) {
           </Link>
           <div className="contact">
             <span>0694 00 00 93</span>
-            <button>Call me</button>
+            <button>Appeller 📞</button>
           </div>
         </div>
       </header>
@@ -40,57 +40,80 @@ export default function Layout({ children, page }) {
             className="img"
             quality={100}
           ></Image>
+          <div className="faya">
+            <h6>
+              © Copyright 2021 -{" "}
+              <a
+                href="https://www.linkedin.com/in/neto-pompeu/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Neto Pompeu
+              </a>
+            </h6>
+          </div>
         </div>
-        <div className="services-">
+        <div className="services">
           <h3>Nos services</h3>
-          <ul className="List-1">
-            <li>
-              <h6>Couverture-toiture</h6>
-            </li>
-            <li>
-              <h6>Jardin et extérieur</h6>
-            </li>
-            <li>
-              <h6>Electricité</h6>
-            </li>
-            <li>
-              <h6>Isolation</h6>
-            </li>
-            <li>
-              <h6>Peinture</h6>
-            </li>
-            <li>
-              <h6>Plomberie</h6>
-            </li>
-          </ul>
+          <div className="list-services">
+            <ul>
+              <li>
+                <h6>Couverture-toiture</h6>
+              </li>
+              <li>
+                <h6>Jardin et extérieur</h6>
+              </li>
+              <li>
+                <h6>Electricité</h6>
+              </li>
+              <li>
+                <h6>Isolation</h6>
+              </li>
+              <li>
+                <h6>Peinture</h6>
+              </li>
+              <li>
+                <h6>Plomberie</h6>
+              </li>
+            </ul>
 
-          <ul className="List-2">
-            <li>
-              <h6>Façadier</h6>
-            </li>
-            <li>
-              <h6>Maçon</h6>
-            </li>
-            <li>
-              <h6>Terrassiers</h6>
-            </li>
-            <li>
-              <h6>carreleurs</h6>
-            </li>
-            <li>
-              <h6>Plaquistes - Plâtriers</h6>
-            </li>
-          </ul>
+            <ul>
+              <li>
+                <h6>Façadier</h6>
+              </li>
+              <li>
+                <h6>Maçon</h6>
+              </li>
+              <li>
+                <h6>Terrassiers</h6>
+              </li>
+              <li>
+                <h6>carreleurs</h6>
+              </li>
+              <li>
+                <h6>Plaquistes - Plâtriers</h6>
+              </li>
+            </ul>
+          </div>
         </div>
         <div className="horaire">
-          <ul>
-            <li></li>
-          </ul>
+          <h3>Les horaires</h3>
+          <div className="list-horaire">
+            <ul>
+              <li>✅ Lun 10h-19h</li>
+              <li>✅ Mar 10h-19h</li>
+              <li>✅ Mer 10h-19h</li>
+              <li>✅ jeu 10h-19h</li>
+              <li>✅ Ven 10h-19h</li>
+              <li>✅ Sam 10h-19h</li>
+              <li>❌ Dim fermé</li>
+            </ul>
+          </div>
         </div>
 
         <div className="contact">
           <span>0694 00 00 93</span>
-          <button>Call me</button>
+          <button>Contactez-nous 📞</button>
         </div>
       </footer>
     </div>
